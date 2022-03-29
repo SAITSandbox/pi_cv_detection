52de84d3c998a78adf2d1c53ac656d974a292ead
Sirvan <sirvan3tr@gmail.com>
2022-03-29 19:30:54 +0100

    crypto module

---
+++ crypto/crypto.py
@@ -0,0 +1,20 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
+def aes256_encrypt(data: str, filename: str):
+    """Take a string and encrypt it, then save it.
+
+    :param data: String of dat to be encrypted.
+    :param filename: String of the file name to be saved.
+    :returns:
+    """
+    # Encode the str data into bytes
+    data = str.encode(data)
+    key = get_random_bytes(32)
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+
+    file_out = open(filename, "wb")
+    [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+    file_out.close()
