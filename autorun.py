743cefdc1ffaac2fac51dba80f35f571005c28af
Dan Baggs <baggage@hotmail.co.uk>
2022-03-30 09:05:24 +0100

    detect added to autorun

--- autorun.py
+++ autorun.py
@@ -1,8 +1,14 @@
 # global imports
 from subprocess import call
+from detect import run
 
 # run computer vision scripts
-
+run(model="efficientdet_lite0.tflite",
+    camera_id=0,
+    width=640,
+    height=480,
+    num_threads=4,
+    enable_edgetpu=False)
 
 
 # run wifi hotspot scripts
