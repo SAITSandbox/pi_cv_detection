4960ca93ac850a2d092dd63553b38cf9795c9372
Daniel Baggs <baggage@hotmail.co.uk>
2022-03-30 18:48:23 +0100

    conditional logic for txrx added

--- autorun.py
+++ autorun.py
@@ -1,8 +1,10 @@
 # global imports
 from subprocess import call
 from cv.detect import run
+from txrx.file_transmitter import ClientListener
+from os import listdir
 
-# run computer vision scripts
+"""run computer vision scripts"""
 run(model="cv/truck.tflite",
     camera_id=0,
     width=640,
@@ -11,9 +13,12 @@ run(model="cv/truck.tflite",
     enable_edgetpu=False,
     time_limit=10)
 
-
-# run wifi hotspot scripts
-
-
-# shutdown the pi for later
-#call(["shutdown", "-h", "now"])
+if len(listdir('not_sent')) > 0:
+    """run wifi hotspot scripts"""
+    # listener = ClientListener()
+    # listener.listen_for_client()
+    pass
+else:
+    """shutdown the pi for later"""
+    # call(["shutdown", "-h", "now"])
+    pass
